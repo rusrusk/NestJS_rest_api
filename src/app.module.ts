c16ccbcd8213b287b4090fbee88e5b8e3ce57c04
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-02-26 15:46:26 +0100

    authService setting up login with controller.ts and service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
   controllers: [AppController],
   providers: [AppService],
 })
